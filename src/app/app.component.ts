4c1b302957256aa8e0210894c7703cc60f6e56a4
adama0911 <goudiabyadama0911@gmail.com>
2018-07-06 17:44:04 +0000

    adama-06-07-2018-17h45

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component ,TemplateRef} from '@angular/core';
+import { BsModalService } from 'ngx-bootstrap/modal';
+import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  etapetnt=1;
+  numero:any;
+  nom:any;
+  prenom:any;
+  tel:any;
+  numeroChip:any;
+  numeroCarte:any;
+  nombreDeMois:any;
+  typeDeBouquet:any;
+  numeroassocie:any;
+  OrangeMoneyRadio:any;
+  tigocashRadio:any;
+  emoneyRadio:any;
+  postcashRadio:any;
+  wizallRadio:any;
+
+  modalRef: BsModalRef;
+
+  constructor(private modalService: BsModalService) {}
+ 
+  reinitialiser (){
+    this.numero=undefined;
+    this.nom=undefined;
+    this.prenom=undefined;
+    this.tel=undefined;
+    this.numeroChip=undefined;
+    this.numeroCarte=undefined;
+    this.nombreDeMois=undefined;
+    this.typeDeBouquet=undefined;
+    this.numeroassocie=undefined;
+    this.OrangeMoneyRadio=undefined;
+    this.tigocashRadio=undefined;
+    this.emoneyRadio=undefined;
+    this.postcashRadio=undefined;
+    this.wizallRadio=undefined;
+  }
+
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
+  }
+
+  infoNumeroTnt(){
+     console.log(this.numero);
+     this.etapetnt=2;
+  }
+
+  validerpaiementTnt(){
+    this.reinitialiser();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import  { FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
+
+import { TabsModule } from 'ngx-bootstrap';
+import { ModalModule } from 'ngx-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    TabsModule.forRoot(),
+    ModalModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
